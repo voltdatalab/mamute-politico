d881e30ff3ac0cbab79139d59ecddac71e3dcca7
JMallone <michels@alunos.utfpr.edu.br>
2026-01-27 15:09:06 -0300

    Add roll call votes and transcripts

--- mamute_scrappers/senado_crawler/__init__.py
+++ mamute_scrappers/senado_crawler/__init__.py
@@ -2,9 +2,11 @@
 
 from .parliamentarian import parliamentarian
 from .proposition import proposition
+from .roll_call_votes import roll_call_votes
 from .proposition_status import proposition_status
 from .proposition_type import proposition_type
+from .speechs_transcipts import speechs_transcipts
 
-__all__ = ["parliamentarian", "proposition", "proposition_status", "proposition_type"]
+__all__ = ["parliamentarian", "proposition", "proposition_status", "proposition_type", "roll_call_votes", "speechs_transcipts"]
 
 
